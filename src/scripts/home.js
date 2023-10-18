6a97029db764939ad5e92cef8a2e8cec9e8d121e
Emily Hawkins <EmilyHawkins0110@gmail.com>
2023-10-17 18:15:37 -0700

    chart.js setup

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,3 +1,7 @@
+//import * from '../../node_modules/chart.js/auto';
+
+window.onload = createChart();
+
 function handleFormSubmission(event) {
     event.preventDefault(); // Prevent the form from submitting
 
@@ -11,13 +15,13 @@ function handleFormSubmission(event) {
     localStorage.setItem("unit", unit.value);
 
     content.innerHTML = ""; // Clear the content div
-    
-    let description = "Nice work! You were in the top "+ localStorage.getItem("input") + "% of composters in Santa Clara.";
+
+    let description = "Nice work! You were in the top " + localStorage.getItem("input") + "% of composters in Santa Clara.";
     content.appendChild(createInfoContainerElement(description));
 
     let stat = "1.5";
     let desc = "kg of CO2 saved";
-    
+
     content.appendChild(createStatsContainerElement(stat, desc));
 
     let stat2 = "3";
@@ -30,7 +34,7 @@ function createInfoContainerElement(description) {
     var display = document.createElement("div");
     display.innerHTML = description;
     display.classList.add("bg-green-100", "border-l-4", "border-green-500", "text-green-700", "rounded", "p-4", "mt-4", "md:w-3/12", "w-4/5", "mx-auto");
-    
+
     var info = document.createElement("p");
     info.classList.add("font-bold", "text-center");
     display.appendChild(info);
@@ -38,28 +42,28 @@ function createInfoContainerElement(description) {
     return display;
 }
 
-function createStatsContainerElement(statistic, description){
+function createStatsContainerElement(statistic, description) {
     var container = document.createElement("div");
-    container.classList.add("p-8", "bg-gray-100", "border", "border-gray-300", "rounded-3xl", "w-4/5", "md:w-3/12","mx-auto", "mt-2");
-    
+    container.classList.add("p-8", "bg-gray-100", "border", "border-gray-300", "rounded-3xl", "w-4/5", "md:w-3/12", "mx-auto", "mt-2");
+
     var stat = document.createElement("p");
-    stat.classList.add("font-heading", "mb-6", "text-3xl", "md:text-6xl", "text-anr-blue", "font-black", "tracking-tight" );
+    stat.classList.add("font-heading", "mb-6", "text-3xl", "md:text-6xl", "text-anr-blue", "font-black", "tracking-tight");
     stat.innerHTML = statistic;
-    
+
     var desc = document.createElement("p");
     desc.classList.add("font-heading", "mb-2", "text-l", "text-gray-700", "font-bold");
     desc.innerHTML = description;
 
     container.appendChild(stat);
-    container.appendChild(desc);    
+    container.appendChild(desc);
 
     return container;
 }
 
-function createTwoStatsRowContainer(stat1, stat2, desc1, desc2){
+function createTwoStatsRowContainer(stat1, stat2, desc1, desc2) {
     var container = document.createElement("div");
     container.classList.add("flex", "w-4/5", "md:w-3/12", "mx-auto");
-    
+
     var statCont1 = createStatsContainerElement(stat1, desc1);
     statCont1.classList.remove("md:w-3/12")
     statCont1.classList.add("mr-1", "md:w-1/2");
@@ -71,4 +75,32 @@ function createTwoStatsRowContainer(stat1, stat2, desc1, desc2){
     container.appendChild(statCont2);
 
     return container;
+}
+
+function createChart() {
+    
+    const ctx = document.getElementById('myChart');
+    console.log("hi");
+    var leChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    leChart.options.aspectRatio = 1;
+    leChart.options.maintainAspectRatio = true;
+    ctx.parentNode.style.height = "300px";
+    ctx.parentNode.style.width = "300px";
 }
